14d91170ac604dba8a7e0627152cb5ef772d5fb3
Mark Gandy <markgandy@fastmail.com>
2018-12-11 13:13:13 -0500

    Use standard layout for 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,10 +3,22 @@ import Layout from '../components/Layout'
 
 const NotFoundPage = () => (
   <Layout>
-    <div>
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    </div>
+    <section className="section section--gradient">
+      <div className="container">
+        <div className="section">
+          <div className="columns">
+            <div className="column is-10 is-offset-1">
+              <div className="content">
+                <h2 className="has-text-weight-semibold is-size-3">
+                  Not Found
+                </h2>
+                <p className="is-size-5">You just hit a route that doesn&#39;t exist... the sadness.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   </Layout>
 )
 
